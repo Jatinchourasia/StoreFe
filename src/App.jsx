57142dd993872e6a78f40e9c6689bf2256702b68
Jatinchourasia <jatinchourasia3@gmail.com>
2023-04-25 15:06:42 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+import data from "./data/data";
+
+function App() {
+  console.log(data);
+  return (
+    <>
+      <h1>heey</h1>
+    </>
+  );
+}
+
+export default App;
